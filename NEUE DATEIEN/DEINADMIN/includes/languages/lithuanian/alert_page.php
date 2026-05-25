eba7c03390824dd6cef56c58da2710745dbeb885
Jörg Schönberger <office@webchills.at>
2026-05-25 16:19:23 +0200

    Version 1.0.2

--- NEUE DATEIEN/DEINADMIN/includes/languages/lithuanian/alert_page.php
+++ NEUE DATEIEN/DEINADMIN/includes/languages/lithuanian/alert_page.php
@@ -1,6 +1,6 @@
 <?php
 /** 
- * @copyright Copyright 2003-2024 Zen Cart Development Team
+ * @copyright Copyright 2003-2022 Zen Cart Development Team
  * Zen Cart German Version - www.zen-cart-pro.at
  * @license https://www.zen-cart-pro.at/license/3_0.txt GNU General Public License V3.0
  * @version $Id: alert_page.php 2022-04-17 16:09:43Z webchills $
--- NEUE DATEIEN/DEINADMIN/includes/languages/lithuanian/extra_definitions/editors_list.php
+++ NEUE DATEIEN/DEINADMIN/includes/languages/lithuanian/extra_definitions/editors_list.php
@@ -2,7 +2,7 @@
 /**
  * Zen Cart German Specific
  * @package htmleditors
- * @copyright Copyright 2003-2024 Zen Cart Development Team
+ * @copyright Copyright 2003-2022 Zen Cart Development Team
  * Zen Cart German Version - www.zen-cart-pro.at
  * @license https://www.zen-cart-pro.at/license/3_0.txt GNU General Public License V3.0
  * @version $Id: editors_list.php 2021-12-01 09:49:16Z webchills $
